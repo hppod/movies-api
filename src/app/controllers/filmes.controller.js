e9583989402e283a2c2ea118cab0d97f1159b41c
hppod <hppodgornik@gmail.com>
2020-08-17 20:36:38 -0300

    Atualizando um filme

--- src/app/controllers/filmes.controller.js
+++ src/app/controllers/filmes.controller.js
@@ -52,5 +52,17 @@ class Filme {
         })
     }
 
+    atualizarUmFilme(req, res) {
+        const nome = req.params.nome
+
+        filmeschema.updateOne({ nome: nome }, { $set: req.body }, (err, data) => {
+            if (err) {
+                res.status(500).send({ message: "Houve um erro ao processar a sua requisição", error: err })
+            } else {
+                res.status(200).send({ message: `Filme ${nome} foi atualizado com sucesso` })
+            }
+        })
+    }
+
 }
 module.exports = new Filme()
--- src/app/routes/filmes.routes.js
+++ src/app/routes/filmes.routes.js
@@ -6,5 +6,6 @@ const Filme = require('./../controllers/filmes.controller')
 route.post('/criar', Filme.criarFilme)
 route.get('/visualizarTodos', Filme.visualizarFilmes)
 route.get('/visualizarUm/:nome', Filme.visualizarUmFilme)
+route.put('/atualizarUm/:nome', Filme.atualizarUmFilme)
 
 module.exports = route
