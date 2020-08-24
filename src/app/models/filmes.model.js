00a8730ad24393468c4a4a5e2f229bda5cf1dfab
hppod <hppodgornik@gmail.com>
2020-08-24 20:54:28 -0300

    Atualizando o model de filmes

--- src/app/models/filmes.model.js
+++ src/app/models/filmes.model.js
@@ -8,15 +8,32 @@ const FilmeSchema = new Schema({
     },
     genero: {
         type: String,
-        required: false,
+        required: true,
         trim: true
     },
     ano: {
         type: Number,
         required: false
     },
-    maior18: {
-        type: Boolean,
+    classificacaoIndicativa: {
+        type: String,
+        required: false
+    },
+    estudio: {
+        type: String,
+        trim: true
+    },
+    duracao: {
+        type: Number
+    },
+    imagem: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    diretor: {
+        type: Schema.Types.ObjectId,
+        ref: 'Diretor',
         required: true
     }
 },
@@ -25,4 +42,4 @@ const FilmeSchema = new Schema({
         versionKey: false
     }
 )
-module.exports = model('filmeschema', FilmeSchema)
+module.exports = model('Filme', FilmeSchema)
