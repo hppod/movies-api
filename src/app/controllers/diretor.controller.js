8800d4cae1272cf82f2ed11b004ed1a313eedfda
hppod <hppodgornik@gmail.com>
2020-08-24 21:48:27 -0300

    Implementando o método de buscar os filmes de um diretor pelo nome dele

--- src/app/controllers/diretor.controller.js
+++ src/app/controllers/diretor.controller.js
@@ -18,5 +18,29 @@ class Diretor {
             })
     }
 
+    buscarOsFilmesDeUmDiretorPeloNomeDele(req, res) {
+        const { nomeDiretor } = req.params
+
+        if (nomeDiretor == undefined || nomeDiretor == 'null') {
+            res.status(400).send({ message: "O nome do diretor deve ser obrigatoriamente preenchido" })
+        }
+
+        diretor.find({ nome: nomeDiretor })
+            .populate('filmes', { nome: 1, imagem: 1 })
+            .exec((err, data) => {
+                if (err) {
+                    res.status(500).send({ message: "Houve um erro ao processar a sua requisição", error: err })
+                } else {
+                    if (data.length <= 0) {
+                        res.status(200).send({ message: `O diretor ${nomeDiretor} não existe no banco de dados` })
+                    } else if (data['filmes'].length <= 0) {
+                        res.status(200).send({ message: `O diretor ${nomeDiretor} não possui nenhum filme cadastrado` })
+                    } else {
+                        res.status(200).send({ message: `O diretor ${nomeDiretor} possui filmes cadastrados`, data: data })
+                    }
+                }
+            })
+    }
+
 }
 module.exports = new Diretor()
